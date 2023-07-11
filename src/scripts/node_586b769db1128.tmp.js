c838fc403c6bb301c89fe8e45f341b30326b375c
Rafael Souza Marques <rsmarques128@gmail.com>
2023-07-11 15:35:08 -0300

    projeto acabado

---
+++ src/scripts/node_586b769db1128.tmp.js
@@ -0,0 +1,8 @@
+//AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2023, 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+setInterval(function() {
+    console.log('ola');
+}, 1000)
